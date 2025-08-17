a8046df9d53a52151e9e74f282c356c0541d751f
TheLim148 <ruzhov34@gmail.com>
2025-08-17 21:16:35 +0300

    I can print necessary information and split it with " and :

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
 import os
-files = os.listdir
 
-with open("temp/2025-01-11.md", "r") as f:
-    for line in f:
-        if line.find("Title") > 0 or line.find(" date") > 0:
-            print(line.strip())
+files = os.listdir("temp")
+
+for file in files:
+    print(f"\n{file}")
+    with open(f"temp/{file}", "r") as f:
+        for line in f:
+            if line.find("Title") > 0:
+                print(line.strip())
+                # print(line.split("\""))
+                # print(line.split(":"))
+            if line.find(" date") > 0:
+                print(line.strip())
