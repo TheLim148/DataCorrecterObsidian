611eff06d52f12d200faf4e9f22590aba793f125
TheLim148 <ruzhov34@gmail.com>
2025-08-17 20:41:29 +0300

    Import os

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
+import os
+files = os.listdir
+
 with open("temp/2025-01-11.md", "r") as f:
     for line in f:
-        if line.find("Title") > 0 or line.find(" date:") > 1:
+        if line.find("Title") > 0 or line.find(" date") > 0:
             print(line.strip())
