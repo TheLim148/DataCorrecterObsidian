d76f2d5e680f88f4c938cefd4b525c261d681eec
TheLim148 <ruzhov34@gmail.com>
2025-08-18 14:47:50 +0300

    getting date from title

--- src/main.py
+++ src/main.py
@@ -1,29 +1,25 @@
 import os
 
 def print_files_information() -> list:
-    # info = []
     files: list = os.listdir("bak")
 
     for file in files:
-        # info.append(file)
         print(f"\n{file}")
         with open(f"bak/{file}", "r+") as text:
             for line in text:
                 if line.find("Title") > 0:
-                    line = line.replace("\"", "")
-                    # info.append(line.strip())
+                    line = line.replace("\"", "").replace(":", "", 1)
+                    date = line.split(":")[1].replace("\n", "").strip()
+                    print(date)
                     print(line.strip())
                     # print(line.split("\""))
                     # print(line.split(":"))
                 if line.find(" date") > 0:
-                    # info.append(line.strip())
                     print(line.strip())
     # return info
 
 def main() -> None:
     print_files_information()
-    # info = print_file_information()
-    # print(info)
 
 if __name__ == "__main__":
     main()
