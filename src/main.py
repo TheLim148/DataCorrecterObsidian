bfbf126ad0b531756e9c31e6e9ea7e3c025b4262
TheLim148 <ruzhov34@gmail.com>
2025-08-18 13:19:10 +0300

    Added some functions stuff

--- src/main.py
+++ src/main.py
@@ -1,14 +1,28 @@
 import os
 
-files = os.listdir("temp")
+def print_files_information() -> list:
+    # info = []
+    files: list = os.listdir("temp")
 
-for file in files:
-    print(f"\n{file}")
-    with open(f"temp/{file}", "r") as f:
-        for line in f:
-            if line.find("Title") > 0:
-                print(line.strip())
-                # print(line.split("\""))
-                # print(line.split(":"))
-            if line.find(" date") > 0:
-                print(line.strip())
+    for file in files:
+        # info.append(file)
+        print(f"\n{file}")
+        with open(f"temp/{file}", "r") as text:
+            for line in text:
+                if line.find("Title") > 0:
+                    # info.append(line.strip())
+                    print(line.strip())
+                    # print(line.split("\""))
+                    # print(line.split(":"))
+                if line.find(" date") > 0:
+                    # info.append(line.strip())
+                    print(line.strip())
+    # return info
+
+def main() -> None:
+    print_files_information()
+    # info = print_file_information()
+    # print(info)
+
+if __name__ == "__main__":
+    main()
