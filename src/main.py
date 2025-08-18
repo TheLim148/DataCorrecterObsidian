9fdc28e373219529778a6929f94bbc88ad512cd7
TheLim148 <ruzhov34@gmail.com>
2025-08-18 20:14:40 +0300

    Now I can get the required line to me, like 'YYYY-MM-DDTHH:mm'

--- src/main.py
+++ src/main.py
@@ -17,30 +17,61 @@ def print_files_information() -> None:
                 if line.find(" date") > 0:
                     print(line.strip())
 
-def foo() -> None:
+def foo() -> list:
     files: list = os.listdir("bak")
 
     new: list = []
+    global date
+    global clean_time
 
     for file in files:
         print(f"\n{file}")
+
         with open(f"bak/{file}", "r", encoding="utf-8") as text:
             list_of_lines = text.readlines()
         
-        for element in list_of_lines:
-            strip_element = element.rstrip("\n")
-            if strip_element.startswith("\"Title"):
-                print(strip_element)
+        for elem in list_of_lines:
+            strip_elem = elem.rstrip("\n")
 
-            if strip_element.startswith("\"Mod"):
-                print(strip_element)
+            if not strip_elem.startswith("\"Title"):
+                pass
 
-            if strip_element.startswith("\"Creation") or strip_element.startswith("Creation"):
-                print(strip_element)
-        # print(lines)
+            if strip_elem.startswith("\"Title"):
+                strip_elem = strip_elem.replace("\"", "").replace(":", "", 1)
+
+                date = strip_elem.split(":")[1].replace("\n", "").strip()
+                # print(strip_elem)
+
+            if strip_elem.startswith("\"Modification date"):
+                strip_elem = strip_elem.replace("\"", "").replace(":", "", 1)
+
+                raw_time = strip_elem.split("2025")[-1].strip().split(":")[0:2]
+                clean_time = ":".join(raw_time)
+
+                # print(clean_time)
+                # print(strip_elem)
+
+            if strip_elem.startswith("Creation date"):
+                pass
+                # print(strip_elem)
+
+            if strip_elem.startswith("\"Creation date"):
+                strip_elem = strip_elem.replace("\"", "").replace(":", "", 1)
+                # print(strip_elem)
+
+            # new.append(strip_elem)
+    # return new
+        
 
 def main() -> None:
+    # foo1: list = foo()
     foo()
+    # print(date)
+    
+    ideal_string = date + "T" + clean_time
+    print(ideal_string)
+
+    # print(foo1)
     # print_files_information()
     
 
