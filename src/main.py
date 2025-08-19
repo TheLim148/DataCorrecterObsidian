edbc6ac80a824fbf095223689051bc111de48576
TheLim148 <ruzhov34@gmail.com>
2025-08-19 15:19:53 +0300

    Make it more cleaner. All that remains is to save the file

--- src/main.py
+++ src/main.py
@@ -1,56 +1,58 @@
 import os
 import re
 
-def foo() -> list:
+def swap(arr, index1, index2):
+    arr[index1], arr[index2] = arr[index2], arr[index1]
+
+def edit_files() -> list:
     global date;       date = ""
     global clean_time; clean_time = ""
     
-    pattern = re.compile(
+    pattern: str = re.compile(
         r"^Creation date: \d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$",
         re.MULTILINE
     )
 
     files: list = os.listdir("bak")
 
-    new: list = []
-
     for file in files:
         with open(f"bak/{file}", "r", encoding="utf-8") as text:
             list_of_lines = text.readlines()
             text.seek(0)
             whole_text = text.read().strip()
-        
-        match = pattern.search(whole_text)
 
+        new: list = []
+
+        match = pattern.search(whole_text)
         if match:
             print(f"\n{file}: соответствует формату")
-            pass
         else:
             print(f"\n{file}")
+            swap(list_of_lines, 4, 5) #Now string "Creation date" above string "Modification date" in array
             for line in list_of_lines:
-                elem = line.strip()
-    
-                if elem.startswith("\"Title"):
-                    elem = elem.replace("\"", "").replace(":", "", 1)
-                    date = elem.split(":")[1].replace("\n", "").strip()
+                elem = line.rstrip("\n")
 
-                if elem.startswith("\"Modification date"):
-                    elem = elem.replace("\"", "").replace(":", "", 1)
+                if elem.startswith("\"Title"): # index = 3
+                    date = elem.split(":")[2].replace("\n", "").strip()
+                    continue
+
+                if elem.startswith("\"Modification date"): #index = 4
                     raw_time = elem.split("2025")[-1].strip().split(":")[0:2]
                     clean_time = ":".join(raw_time) + ":00"
+                    continue
 
-                if elem.startswith("\"Creation date"):
+                if elem.startswith("\"Creation date"): # index = 5
                     elem = elem.replace("\"", "").replace(":", "", 1)
-
-            format_string = date + "T" + clean_time
-            print(format_string)
-            # new.append(elem)
-        
+                    elem = elem.split(":")[0] + ": "
+                    format_string = date + "T" + clean_time
+                    elem += format_string
+                new.append(elem + "\n")
+            # print(new)
     return new
         
 
 def main() -> None:
-    foo()
+    edit_files()
 
 if __name__ == "__main__":
     main()
